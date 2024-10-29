2b6fef49b5dad770e5651e929172fae3610c497c
Brian Nguyen <briannguyenmain@gmail.com>
2024-10-29 19:06:49 -0400

    Created the game class and defined the init method

--- buckshot_roulette.py
+++ buckshot_roulette.py
@@ -1,1 +1,11 @@
 "The main file for Buckshot Roulette."
+
+class game:
+    """The game class which will lay the foundation to start the buckshot roulette game.
+    
+    Attributes:
+        -----
+    """
+    def __init__(self):
+        pass
+    
