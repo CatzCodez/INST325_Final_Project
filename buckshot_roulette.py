d890b3bcc58df7dc6fa9db2bf32396d0b56dec6e
Brian Nguyen <briannguyenmain@gmail.com>
2024-10-29 19:45:44 -0400

    fixed spacing and indentation

--- buckshot_roulette.py
+++ buckshot_roulette.py
@@ -1,10 +1,11 @@
 "The main file for Buckshot Roulette."
 
-class game:
-    """The game class which will lay the foundation to start the buckshot roulette game.
+class Game:
+    """
+    The game class which will lay the foundation to start the buckshot roulette game.
     
     Attributes:
-        -----
+        
     """
     def __init__(self):
         pass
@@ -16,6 +17,7 @@ class game:
         Returns:
             ???: Current player's action
         """
+        pass
     
     def check_game_status(self, player):
         """
@@ -23,10 +25,12 @@ class game:
         
         Args: 
             player(player): Player taking turn
+            
         Returns: 
             game Status
         """
         pass
+    
     def loot_box(player):
         """
         Will choose up to 4 random items for player.
@@ -35,6 +39,8 @@ class game:
         Side effect:
             A new item for the player
         """
+        pass
+    
     def shoot_target(player, target, knife = False):
         """
         Executes a shot from the player to the target
@@ -43,6 +49,7 @@ class game:
             player: the current player whose turn it is
             target: whom the player will shoot at
             knife: whether the next shot will do twice as much damage. Automatically set to false
+            
         Return:
         """
         pass
