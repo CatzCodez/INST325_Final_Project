11a31352e4059d197907219b688476ba29f4da0b
Brian Nguyen <briannguyenmain@gmail.com>
2024-11-04 15:15:53 -0500

    Restructuring classes

--- buckshot_roulette.py
+++ buckshot_roulette.py
@@ -1,55 +1,12 @@
-"The main file for Buckshot Roulette."
+class Player:
+    pass
 
-class Game:
-    """
-    The game class which will lay the foundation to start the buckshot roulette game.
-    
-    Attributes:
-        
-    """
-    def __init__(self):
-        pass
-    def take_turn(player):
-        """ Allows a player to take a turn
-    
-        Args: 
-            player (player): Current player taking turn
-        Returns:
-            ???: Current player's action
-        """
-        pass
-    
-    def check_game_status(self, player):
-        """
-        Checks status of game
-        
-        Args: 
-            player(player): Player taking turn
-            
-        Returns: 
-            game Status
-        """
-        pass
-    
-    def loot_box(player):
-        """
-        Will choose up to 4 random items for player.
-        
-        Args:
-        Side effect:
-            A new item for the player
-        """
-        pass
-    
-    def shoot_target(player, target, knife = False):
-        """
-        Executes a shot from the player to the target
-        
-        Args:
-            player: the current player whose turn it is
-            target: whom the player will shoot at
-            knife: whether the next shot will do twice as much damage. Automatically set to false
-            
-        Return:
-        """
-        pass
+class Item:
+    pass
+
+class BuckshotRoulette:
+    pass
+
+
+if __name__ = "__main__":
+    pass
